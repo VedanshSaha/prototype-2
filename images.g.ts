392af0d6dfc66b06f893f179bc2ef7c4aeb4b9fb
VedanshSaha <76149219+VedanshSaha@users.noreply.github.com>
2022-05-04 16:25:12 +0530

    prototype - 3.84

--- images.g.ts
+++ images.g.ts
@@ -275,48 +275,31 @@ f f f f f f f f f f f f f f f f
 f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f 
 `;
+            case "image9":
+            case "myImage4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
         }
         return null;
     })
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "myAnim":
-            case "anim1":return [img`
-. . . . . . f f f f f f . . . . 
-. . . . . f e e e e e e f . . . 
-. . . . f e e e e e e e e f . . 
-. . . f e e e e e e e e e e f . 
-. . . f e e e f e e e e f e f . 
-. . . f e e e e f e e f e e f . 
-. . . f e e e e e e e e e e f . 
-. . f f e e e f e e e e e f . . 
-. f e e f e e e f f f f f . . . 
-f e e e e f f e e e e 2 e f . . 
-f f e e e e f f f e e 2 e e f . 
-. . f e e e f f f e e 1 1 1 1 f 
-. . f e e e e f e e 1 1 1 1 1 f 
-. . f f e e e e e 1 1 1 1 1 e f 
-. . . f e e e 1 1 1 1 1 1 e f . 
-. . . . f f f f f f f f f f . . 
-`, img`
-. . . . . . . f f f f f f . . . 
-. . . . . . f e e e e e e f . . 
-. . . . . f e e e e e e e e f . 
-. . . . f e e e e e e e e e e f 
-. . . . f e e e f e e e e f e f 
-. . . . f e e e e f e e f e e f 
-. . . . f e e e e e e e e e e f 
-. . . . f e e e f e e e e e f . 
-. . f f f f e e e f f f f f . . 
-. f e e e f f f e e e 2 e f . . 
-f e e e e e f f e e 2 e e e f . 
-f f f e e e f e e e 1 1 1 1 f . 
-. . f e e e e e e 1 1 1 1 1 f . 
-. . f e e e e e 1 1 1 1 1 e f . 
-. . f f e e e 1 1 1 1 1 e f . . 
-. . . f f f f f f f f f f . . . 
-`];
             case "myAnim1":
             case "anim3":return [img`
 . . . . . . . . . . b 5 b . . . 
@@ -513,95 +496,6 @@ c b d d d d d 5 5 5 5 5 5 5 b .
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`];
-            case "myAnim0":
-            case "anim2":return [img`
-. . . . f f f f f f . . . . . . 
-. . . f e e e e e e f . . . . . 
-. . f e e e e e e e e f . . . . 
-. f e e e e e e e e e e f . . . 
-. f e f e e e e f e e e f . . . 
-. f e e f e e f e e e e f . . . 
-. f e e e e e e e e e e f . . . 
-. . f e e e e e f e e e f f . . 
-. . . f f f f f e e e f e e f . 
-. . f e 2 e e e e f f e e e e f 
-. f e e 2 e e f f f e e e e f f 
-f 1 1 1 1 e e f f f e e e f . . 
-f 1 1 1 1 1 e e f e e e e f . . 
-f e 1 1 1 1 1 e e e e e f f . . 
-. f e 1 1 1 1 1 1 e e e f . . . 
-. . f f f f f f f f f f . . . . 
-`, img`
-. . . f f f f f f . . . . . . . 
-. . f e e e e e e f . . . . . . 
-. f e e e e e e e e f . . . . . 
-f e e e e e e e e e e f . . . . 
-f e f e e e e f e e e f . . . . 
-f e e f e e f e e e e f . . . . 
-f e e e e e e e e e e f . . . . 
-. f e e e e e f e e e f . . . . 
-. . f f f f f e e e f f f f . . 
-. . f e 2 e e e f f f e e e f . 
-. f e e e 2 e e f f e e e e e f 
-. f 1 1 1 1 e e e f e e e f f f 
-. f 1 1 1 1 1 e e e e e e f . . 
-. f e 1 1 1 1 1 e e e e e f . . 
-. . f e 1 1 1 1 1 e e e f f . . 
-. . . f f f f f f f f f f . . . 
-`];
-            case "CamelGif":
-            case "anim9":return [img`
-................................
-................................
-...........eeee.eee.............
-...........eeee.eeee.....ee.....
-........eeeeeee.eeee.....efee...
-........eeeeeeeeeeeee..eeeeee...
-........eeeeeeeeeeeeeeeeee......
-....eeeeeeeeeeeeeeeeeeeee.......
-.......eeeeeeeeeeeeeeee.........
-.......eeeeeeeeeeeeeeee.........
-........eeeeeeeeeeeeeee.........
-.........eeeee..eeeeee..........
-...........e.......e............
-...........e.......e............
-...........e.......e............
-...........e.......e............
-`, img`
-................................
-................................
-...........eeee.eee.............
-...........eeee.eeee.....ee.....
-........eeeeeee.eeee.....efee...
-........eeeeeeeeeeeee..eeeeee...
-....eee.eeeeeeeeeeeeeeeeee......
-...ee.eeeeeeeeeeeeeeeeeee.......
-.......eeeeeeeeeeeeeeee.........
-.......eeeeeeeeeeeeeeee.........
-........eeeeeeeeeeeeeee.........
-.........eeeee..eeeeee..........
-...........e.......e............
-...........e.......e............
-...........e.......e............
-...........e.......e............
-`, img`
-................................
-................................
-...........eeee.eee.............
-...........eeee.eeee............
-........eeeeeee.eeee.....ee.....
-........eeeeeeeeeeeee....efee...
-....eee.eeeeeeeeeeeeeeeeeeeee...
-...ee.eeeeeeeeeeeeeeeeeeee......
-..ee...eeeeeeeeeeeeeeeeee.......
-.......eeeeeeeeeeeeeeee.........
-........eeeeeeeeeeeeeee.........
-.........eeeee..eeeeee..........
-...........e.......e............
-...........e.......e............
-...........e.......e............
-...........e.......e............
 `];
             case "myAnim4":
             case "anim6":return [img`
@@ -1039,6 +933,165 @@ f f f e e e f e e f f f . . . .
 ..................................................
 ..................................................
 ..................................................
+`];
+            case "myAnim0":
+            case "anim2":return [img`
+. . f f f . . . . . . . . f f f 
+. f f c c . . . . . . f c b b c 
+f f c c . . . . . . f c b b c . 
+f c f c . . . . . . f b c c c . 
+f f f c c . c c . f c b b c c . 
+f f c 3 c c 3 c c f b c b b c . 
+f f b 3 b c 3 b c f b c c b c . 
+. c b b b b b b c b b c c c . . 
+. c 1 b b b 1 b b c c c c . . . 
+c b b b b b b b b b c c . . . . 
+c b c b b b c b b b b f . . . . 
+f b 1 f f f 1 b b b b f c . . . 
+f b b b b b b b b b b f c c . . 
+. f b b b b b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+`, img`
+. . f f f . . . . . . . . . . . 
+f f f c c . . . . . . . . f f f 
+f f c c . . c c . . . f c b b c 
+f f c 3 c c 3 c c f f b b b c . 
+f f b 3 b c 3 b c f b b c c c . 
+. c b b b b b b c f b c b c c . 
+. c b b b b b b c b b c b b c . 
+c b 1 b b b 1 b b b c c c b c . 
+c b b b b b b b b c c c c c . . 
+f b c b b b c b b b b f c . . . 
+f b 1 f f f 1 b b b b f c c . . 
+. f b b b b b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . c c . . c c . . . . . . . . 
+. . c 3 c c 3 c c c . . . . . . 
+. c b 3 b c 3 b c c c . . . . . 
+. c b b b b b b b b f f . . . . 
+c c b b b b b b b b f f . . . . 
+c b 1 b b b 1 b b c f f f . . . 
+c b b b b b b b b f f f f . . . 
+f b c b b b c b c c b b b . . . 
+f b 1 f f f 1 b f c c c c . . . 
+. f b b b b b b f b b c c . . . 
+c c f b b b b b c c b b c . . . 
+c c c f f f f f f c c b b c . . 
+. c c c . . . . . . c c c c c . 
+. . c c c . . . . . . . c c c c 
+. . . . . . . . . . . . . . . . 
+`, img`
+. f f f . . . . . . . . f f f . 
+f f c . . . . . . . f c b b c . 
+f c c . . . . . . f c b b c . . 
+c f . . . . . . . f b c c c . . 
+c f f . . . . . f f b b c c . . 
+f f f c c . c c f b c b b c . . 
+f f f c c c c c f b c c b c . . 
+. f c 3 c c 3 b c b c c c . . . 
+. c b 3 b c 3 b b c c c c . . . 
+c c b b b b b b b b c c . . . . 
+c b 1 b b b 1 b b b b f c . . . 
+f b b b b b b b b b b f c c . . 
+f b c b b b c b b b b f . . . . 
+. f 1 f f f 1 b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+`];
+            case "myAnim":
+            case "anim1":return [img`
+. . . . f f f f f f . . . . . . 
+. . . f e e e e e e f . . . . . 
+. . f e e e e e e e e f . . . . 
+. f e e e e e e e e e e f . . . 
+. f e f e e e e f e e e f . . . 
+. f e e f e e f e e e e f . . . 
+. f e e e e e e e e e e f . . . 
+. . f e e e e e f e e e f f . . 
+. . . f f f f f e e e f e e f . 
+. . f e 2 e e e e f f e e e e f 
+. f e e 2 e e f f f e e e e f f 
+f 1 1 1 1 e e f f f e e e f . . 
+f 1 1 1 1 1 e e f e e e e f . . 
+f e 1 1 1 1 1 e e e e e f f . . 
+. f e 1 1 1 1 1 1 e e e f . . . 
+. . f f f f f f f f f f . . . . 
+`, img`
+. . . f f f f f f . . . . . . . 
+. . f e e e e e e f . . . . . . 
+. f e e e e e e e e f . . . . . 
+f e e e e e e e e e e f . . . . 
+f e f e e e e f e e e f . . . . 
+f e e f e e f e e e e f . . . . 
+f e e e e e e e e e e f . . . . 
+. f e e e e e f e e e f . . . . 
+. . f f f f f e e e f f f f . . 
+. . f e 2 e e e f f f e e e f . 
+. f e e e 2 e e f f e e e e e f 
+. f 1 1 1 1 e e e f e e e f f f 
+. f 1 1 1 1 1 e e e e e e f . . 
+. f e 1 1 1 1 1 e e e e e f . . 
+. . f e 1 1 1 1 1 e e e f f . . 
+. . . f f f f f f f f f f . . . 
+`];
+            case "CamelGif":
+            case "anim9":return [img`
+................................
+................................
+.............eee.eeee...........
+.....ee.....eeee.eeee...........
+...eefe.....eeee.eeeeeee........
+...eeeeee..eeeeeeeeeeeee........
+......eeeeeeeeeeeeeeeeee........
+.......eeeeeeeeeeeeeeeeeeeee....
+.........eeeeeeeeeeeeeeee.......
+.........eeeeeeeeeeeeeeee.......
+.........eeeeeeeeeeeeeee........
+..........eeeeee..eeeee.........
+............e.......e...........
+............e.......e...........
+............e.......e...........
+............e.......e...........
+`, img`
+................................
+................................
+.............eee.eeee...........
+.....ee.....eeee.eeee...........
+...eefe.....eeee.eeeeeee........
+...eeeeee..eeeeeeeeeeeee........
+......eeeeeeeeeeeeeeeeee.eee....
+.......eeeeeeeeeeeeeeeeeee.ee...
+.........eeeeeeeeeeeeeeee.......
+.........eeeeeeeeeeeeeeee.......
+.........eeeeeeeeeeeeeee........
+..........eeeeee..eeeee.........
+............e.......e...........
+............e.......e...........
+............e.......e...........
+............e.......e...........
+`, img`
+................................
+................................
+.............eee.eeee...........
+............eeee.eeee...........
+.....ee.....eeee.eeeeeee........
+...eefe....eeeeeeeeeeeee........
+...eeeeeeeeeeeeeeeeeeeee.eee....
+......eeeeeeeeeeeeeeeeeeee.ee...
+.......eeeeeeeeeeeeeeeeee...ee..
+.........eeeeeeeeeeeeeeee.......
+.........eeeeeeeeeeeeeee........
+..........eeeeee..eeeee.........
+............e.......e...........
+............e.......e...........
+............e.......e...........
+............e.......e...........
 `];
         }
         return null;
